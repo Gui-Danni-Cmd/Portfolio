fc08f81be395f8aa24f6a89dae3c5ecc397a7c99
Gui-Danni-Cmd <66735899+Gui-Danni-Cmd@users.noreply.github.com>
2022-11-02 22:24:46 -0300

    Portfolio

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+
+document.querySelector('#app').innerHTML = `
+  <div class="card">
+    <div class="timeline"></div>
+    <div class="img" >
+      <img src="./assets/perfil.jpg">
+    </div>
+  </div>
+`
+
