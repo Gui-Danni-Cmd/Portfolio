fe5fcf89025d332e38a4d5d16e250139c176ccfc
Gui-Danni-Cmd <66735899+Gui-Danni-Cmd@users.noreply.github.com>
2022-11-02 23:28:13 -0300

    deploit

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
 
 document.querySelector('#app').innerHTML = `
   <div class="card">
     <div class="timeline"></div>
     <div class="img" >
       <img src="./assets/perfil.jpg">
+    </div>
+    <div class="conteiner">
+      <div class="descrition">
+          <h2>
+            Guilherme Dannilo<br><span>Dev JavaScript-React UX|UI;</span>
+          </h2>
+          <div class="btn">
+            <i class="fa-brands fa-github"></i>
+          </div>
+      </div>
     </div>
   </div>
 `
